c9c34a670f0d6a6182d55612388eb0f89cc77b61
khushpreet <khushnewton2014@gmail.com>
2023-05-19 15:14:47 +0530

    Trying out git

--- Main.java
+++ Main.java
@@ -3,5 +3,10 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        int sum = 0;
+        for(int i =0;i<5;i++){
+            sum = sum+i;
+        }
+        System.out.println(sum);
     }
 }
